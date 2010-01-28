3742c5ec1c17674e67b66fe40fdb51010dba55e4
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-01-28 10:17:48 +0000

    * adding i18n language resources (template from commit 510)

---
+++ woas/src/i18n.js
@@ -0,0 +1,13 @@
+
+// internationalization language resources
+woas['i18n'] = {
+	READ_ONLY: "This Wiki on a Stick is read-only",
+	INVALID_SPECIAL: "Invalid special page.",
+	INVALID_DATA:"Invalid collected data!",
+	CONFIRM_DELETE:"Are you sure you want to DELETE page",
+	CONFIRM_DELETE_ALL1: "Are you going to ERASE all your pages?",
+	CONFIRM_DELETE_ALL2: "This is the last confirm needed in order to ERASE all your pages.\n\nALL YOUR PAGES WILL BE LOST\n\nAre you sure you want to continue?",
+	DELETE:"Delete page:",
+ 	DELETE_IMAGE:"Delete embedded image",
+	EXPORT_IMAGE:"Export image"
+}
