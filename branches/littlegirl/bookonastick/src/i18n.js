46044b4bb2f5b6c892730f464f4b5455a3b2bff3
littlegirl <littlegirl@dadf181f-332c-0410-97d6-92020e263e48>
2010-01-28 16:45:01 +0000

    * Created the bookonastick branch.

---
+++ branches/littlegirl/bookonastick/src/i18n.js
@@ -0,0 +1,34 @@
+
+// internationalization language resources
+woas['i18n'] = {
+	READ_ONLY: "This Wiki on a Stick is read-only",
+	WRITE_PROTECTED: "Sorry, this Wiki on a Stick is already write-protected",
+	CONFIRM_READ_ONLY: "Are you sure you want to set this WoaS as read-only? You will have to manually edit the file to revert this change.",
+	INVALID_SPECIAL: "Invalid special page.",
+	INVALID_DATA:"Invalid collected data!",
+	CONFIRM_DELETE:"Are you sure you want to DELETE page",
+	CONFIRM_DELETE_ALL1: "Are you going to ERASE all your pages?",
+	CONFIRM_DELETE_ALL2: "This is the last confirm needed in order to ERASE all your pages.\n\nALL YOUR PAGES WILL BE LOST\n\nAre you sure you want to continue?",
+	DELETE:"Delete page:",
+ 	DELETE_IMAGE:"Delete embedded image",
+ 	DELETE_FILE:"Delete embedded file",
+ 	EXPORT_FILE:"Export file",
+ 	EXPORT_IMAGE:"Export image",
+	CONFIRM_DELETE_FILE: "Are you sure you want to delete this file?",
+	INSERT_NEW:"Insert new page title",
+	LOAD_ERR: "Could not load file ",
+	FILE_SELECT_ERR: "A file must be selected",
+	PAGE_NOT_FOUND: "Page not found. Do you want to create it?",
+	CONFIRM_EXPORT: "Do you want to export this file in the below specified path?",
+	ASK_MENU_LINK: "Do you want to add a link into the main menu?",
+	BRACKETS_TITLE: "Cannot use \"[[\" or \"]]\" in a page title",
+	PAGE_EXISTS: "A page with title \"%s\" already exists!",
+	FILE_DISPLAY_LIMIT: "Only the first 1024 bytes are displayed",
+	IMAGE_CONFIRM_EXPORT: "Do you want to export this image in the below specified path?",
+	CONFIRM_DELETE_IMAGE: "Are you sure you want to delete image '%s'?",
+	DISPLAY_FULL_FILE: "Display full file",
+	LOADING: "Loading Wiki on a Stick..."
+}
+
+// do not use any copyrighted list here
+woas.i18n['common_words'] = ['the','of','to' ,'and' ,'a' ,'in' ,'is' ,'it' ,'you' ,'that' ,'he' ,'was' ,'for','on' ,'are' ,'with' ,'as' ,'I' ,'his' ,'they' ,'be' ,'at' ,'one' ,'have' ,'this' ,'from' ,'or' ,'had' ,'by' , 'an', 'all' ];
