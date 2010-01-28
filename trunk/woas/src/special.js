a53a3c30f0e7b52afcc7f190cc6e1046e1550c33
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-01-28 11:28:19 +0000

    * loop optimizations (reviewed from commit 506 by fbnil)

--- trunk/woas/src/special.js
+++ trunk/woas/src/special.js
@@ -2,7 +2,7 @@
 
 woas["special_encrypted_pages"] = function(locked) {
 	var pg = [];
-	for(var i=0;i<pages.length;i++) {
+	for(var i=0,l=pages.length;i<l;i++) {
 		if (locked == this.is__encrypted(i))
 			pg.push(page_titles[i]);
 	}
@@ -12,7 +12,7 @@ woas["special_encrypted_pages"] = function(locked) {
 woas["special_orphaned_pages"] = function() {
 	var pg = [];
 	var found = false;
-	for(j=0; j<page_titles.length; j++) {
+	for(j=0,l=page_titles.length; j<l; j++) {
 		if (this.is_reserved(page_titles[j]))
 			continue;
 		if (this.is_menu(page_titles[j])) {	// check if the namespace has some pages
@@ -27,7 +27,7 @@ woas["special_orphaned_pages"] = function() {
 		} else {
 		// search for pages that link to it
 			var tmp;
-			for(var i=0; i<page_titles.length; i++) {
+			for(var i=0,l=page_titles.length; i<l; i++) {
 				if ((i==j) || this.is_reserved(page_titles[i]))
 					continue;
 				tmp = this.get_src_page(i);
@@ -93,10 +93,11 @@ woas["special_search"] = function( str ) {
 					replace(/\s+/g, ".*?") + ".*", "gi" );	*/
 	var tmp;
 	result_pages = [];
-	for(var i=0; i<pages.length; i++) {
+	for(var i=0,l=pages.length; i<l; i++) {
 		if (this.is_reserved(page_titles[i]))
 			continue;
-		
+
+		// this could be modified for wiki searching issues
 		tmp = this.get_src_page(i);
 		if (tmp==null)
 			continue;
@@ -168,7 +169,7 @@ woas["special_tagged"] = function() {
 woas["special_untagged"] = function() {
 	var tmp;
 	var pg = [];
-	for(var i=0; i<pages.length; i++) {
+	for(var i=0,l=pages.length; i<l; i++) {
 		tmp = this.get_src_page(i);
 		if (tmp==null)
 			continue;
@@ -183,8 +184,7 @@ woas["special_untagged"] = function() {
 // Returns a index of all pages
 woas["special_all_pages"] = function() {
 	var pg = [];
-	for(var i=0; i<page_titles.length; i++)
-	{
+	for(var i=0, l=page_titles.length; i<l; i++) {
 		if (!this.is_reserved(page_titles[i]))
 			pg.push( page_titles[i] );
 	}
@@ -197,7 +197,7 @@ woas["special_dead_pages"] = function() {
 	var from_pages = [];
 	var page_done = false;
 	var tmp;
-	for (j=0;j<pages.length;j++) {
+	for (var j=0,l=pages.length;j<l;j++) {
 		tmp = this.get_src_page(j);
 		if (tmp==null)
 			continue;
@@ -210,6 +210,9 @@ woas["special_dead_pages"] = function() {
 				if ($1.search(/^\w+:\/\//)==0)
 					return;
 				if ($1.match(/Tag(s|ged)?:/gi))
+					return;
+				// skip mailto URLs
+				if ($1.match(/^mailto:/gi))
 					return;
 				p = $1;
 				if (!woas.page_exists(p) && (p!=page_titles[j])) {
