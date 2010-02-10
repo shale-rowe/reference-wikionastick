999bed6acc4d17cb39fe507d5dee04416f251e87
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-02-10 16:11:10 +0000

    * further development in implementation of libwsif

--- trunk/tools/libwsif.php
+++ trunk/tools/libwsif.php
@@ -90,114 +90,96 @@ class WSIF {
 					// we have just jumped over a page definition
 					if ($title !== null) {
 						// store the previously parsed page definition
-						p = this._native_page_def(path,ct,previous_h, p,overwrite,
-								title,attrs,last_mod,len,encoding,disposition,
-								d_fn,boundary,mime);
+						$p = $this->_page_def($path,$ct,$previous_h,$p,
+								$title,$attrs,$last_mod,$len,$encoding,$disposition,
+								$d_fn,$boundary,$mime);
 						// save page index for later analysis
-						var was_title = title;
-						title = attrs = last_mod = encoding = len =
-							 boundary = disposition = mime = d_fn = null;
-						if (p === false) {
-							fail = true;
-							break;
+						$was_title = title;
+						$title = $attrs = $last_mod = $encoding = $len =
+							 $boundary = $disposition = $mime = $d_fn = null;
+						if ($p === false) {
+							$fail = true;
+							break 2;
 						}
 						// check if page was really imported, and if yes then
 						// add page to list of imported pages
-						if (this.wsif.imported_page !== false)
-							imported.push(this.wsif.imported_page);
+						if ($this->_imported_page !== false)
+							$imported[] = $this->_imported_page;
 						else
-							log("Import failure for "+was_title); //log:1
+							$this->_log("Import failure for "+was_title); //log:1
 						// delete the whole entry to free up memory to GC
 						// will delete also the last read header
-						ct = ct.substr(p);
-						p = 0;
-						previous_h = null;
+						$ct = substr($ct, p);
+						$p = 0;
+						$previous_h = null;
 					}
 					// let's start with the next page
-					title = this.ecma_decode(v);
+					$title = $this->_ecma_decode($v);
 				break;
 				case "attributes":
-					attrs = Number(v);
+					$attrs = (int)$v;
 				break;
 				case "last_modified":
-					last_mod = Number(v);
+					$last_mod = (int)$v;
 				break;
 				case "length":
-					len = Number(v);
+					$len = (int)$v;
 				break;
 				case "encoding":
-					encoding = v;
+					$encoding = $v;
 				break;
 				case "disposition":
-					disposition = v;
+					$disposition = $v;
 				break;
 				case "disposition.filename":
-					d_fn = v;
+					$d_fn = $v;
 				break;
 				case "boundary":
-					boundary = v;
+					$boundary = $v;
 				break;
 				case "mime":
-					mime = v;
+					$mime = $v;
 				break;
 				default:
-					log("Unknown WSIF header: "+s);
+					$this->_log("Unknown WSIF header: ".$s);
 			} // end switch(s)
-			if (fail)
-				break;
+//			if ($fail)				break;
 			// set pointer to next entry
-			p = ct.indexOf(pfx, p);
+			$p = strpos($ct, $pfx, $p);
 		}
-	/*	if (recursing) {
-			this.alert("title = "+title+"\nattrs = "+attrs+"\nlast_mod = "+last_mod+"\n"+
-					"len = "+len+"\nencoding = "+encoding+"\ndisposition = "+disposition+
-					"\nboundary = "+boundary+"\n");
-		} */
-		if (fail)
+		if ($fail)
 			return false;
 		// process the last page (if any)
-		if ((previous_h !== null) && (title !== null)) {
-			p = this._native_page_def(path,ct,previous_h,0,overwrite,
-					title,attrs,last_mod,len,encoding,disposition,
-					d_fn,boundary,mime);
+		if (($previous_h !== null) && ($title !== null)) {
+			$p = $this->_page_def($path,$ct,$previous_h,0,
+					$title,$attrs,$last_mod,$len,$encoding,$disposition,
+					$d_fn,$boundary,$mime);
 			// save page index for later analysis
-			if (p === false) {
-				this.wsif.emsg = "Cannot find page "+title+" after import!";
-				fail = true;
+			if ($p === false) {
+				$this->_emsg = sprintf(_WSIF_IMPORT_FAILURE, $title);
+				$fail = true;
 			} else {
 				// check if page was really imported, and if yes then
 				// add page to list of imported pages
-				if (this.wsif.imported_page !== false)
-					imported.push(this.wsif.imported_page);
+				if ($this->_imported_page !== false)
+					$imported[] = $this->_imported_page;
 				else
-					log("Import failure for "+title); //log:1
+					$this->_log("Import failure for "+title); //log:1
 			}
 		}
 		// save imported pages
-		if (imported.length) {
-			if (and_save)
-				this.commit(imported);
-			return imported.length;
-		}
-		// no pages were changed
-		return 0;
+		return count($imported);
 	}
 
-	woas["get_path"] = function(id) {
-		if (ff3 || ff_new)
-			return ff3_getPath($(id));
-		// on older browsers this was allowed
-		return $(id).value;
-	}
-
-	woas["_native_page_def"] = function(path,ct,p,last_p,overwrite, title,attrs,last_mod,len,encoding,
-												disposition,d_fn,boundary,mime) {
-		this.wsif.imported_page = false;
-		if (disposition == "inline") {
+	function _page_def($path,&$ct,$p,$last_p,
+						$title,$attrs,$last_mod,$len,$encoding,
+						$disposition,$d_fn,$boundary,$mime) {
+		$this->_imported_page = false;
+		if ($disposition == "inline") {
 			// craft the exact boundary match string
-			boundary = "\n--"+boundary+"\n";
+			$boundary = "\n--".$boundary."\n";
 			// locate start and ending boundaries
-			var bpos_s = ct.indexOf(boundary, p);
+			$bpos_s = strpos($ct, $boundary, $p);
 			if (bpos_s == -1) {
 				this.wsif.emsg = "Failed to find start boundary "+boundary+" for page "+title;
 				return -1;
