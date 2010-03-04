71cd23272c7e59af95b14b77e70dbf904bc3adad
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-03-04 16:09:09 +0000

    * write tool 1.1.0: allow multiple files

--- tools/wsif_write_example.php
+++ tools/wsif_write_example.php
@@ -2,7 +2,7 @@
 <?php
 ## WSIF write example
 ## @author legolas558
-## @version 1.0.0
+## @version 1.1.0
 ## @copyright GNU/GPL
 ## (c) 2010 Wiki on a Stick project
 ## @url http://stickwiki.sf.net/
@@ -13,43 +13,53 @@
 require dirname(__FILE__).'/libwsif.php';
 
 if ($argc<2) {
-	fprintf(STDERR, "Usage:\n\t%s\tinput_file [\"Page title\"]\n", $argv[0]);
+	fprintf(STDERR, "Usage:\n\t%s\t{input_file [\"Page title\"]} | {[input_file \"Page title\"] [input_file_n] [\"Page title n\"] ... }\n", $argv[0]);
 	exit(-1);
 }
 
-$title = $src = $argv[1];
-
-$src = file_get_contents($src);
-if ($src === FALSE) {
-	exit(-2);
+// build up an array of sources and titles to work on
+global $sources, $titles;
+if ($argc == 2) {
+	$sources = array($argv[1]);
+	$titles = array($argv[1]);
+} else {
+	if (($argc % 2) == 0) {
+		$argv[] = $argv[$argc-1];
+		++$argc;
+	}
+	$sources = array();
+	$titles = Array();
+	for($i=1;$i<$argc;$i+=2) {
+		$sources[] = $argv[$i];
+		$titles[] = $argv[$i+1];
+	}
 }
 
-if ($argc == 3)
-	$title = $argv[2];
-
-global $data;
-$NP = new WoaS_Page();
-$NP->title = $title;
-$NP->content = $src;
-$data = array($NP);
-	
 function example_read_cb() {
-	global $data;
-	if (!count($data))
+	global $sources,$titles;
+	if (!count($titles))
 		return false;
-	$NP = array_pop($data);
+	$src = array_pop($sources);
+	$src = file_get_contents($src);
+	if ($src === FALSE) {
+		array_pop($titles);
+		return false;
+	}
+	$NP = new WoaS_Page();
+	$NP->title = array_pop($titles);
+	$NP->content = $src;
+	$pages[] = $NP;
+
 	return $NP;
 }
 
 $WSIF = new WSIF();
-
 $done = $WSIF->Save('example_read_cb', dirname(__FILE__).'/');
 
 sprintf("%d pages written\n", $done);
 
-if ($done)
-	exit(0);
-	
-exit(-1);
+$done -= count($sources);
+
+exit($done);
 
 ?>
