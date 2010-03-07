953be4adc9ec954527fd00452f053ae815850737
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-03-07 19:02:39 +0000

    * added "ghost page" check when canceling edit of a newly created page * improved cancel edits message (suggested by littlegirl) * changed erasen wiki default main page

--- woas/src/i18n.js
+++ woas/src/i18n.js
@@ -63,7 +63,7 @@ woas['i18n'] = {
 	WSIF_NO_HN: "Could not locate header name",
 	WSIF_BAD_HV: "Could not locate end of header value",
 	ALT_BROWSER_INPUT: "Please specify full path to file \"%s\" because your browser does not allow such path to be read directly for security reasons.",
-	CANCEL_EDITING:"Cancel editing without saving any change?",
+	CANCEL_EDITING:"Changes to this page will not be saved.",
 	TOO_LONG_TITLE: "Maximum title length is %d characters",
 	UNSUPPORTED_BROWSER: "Your browser might not be supported, please report a bug with your UserAgent string:\n%s",
 	SAVE_ERROR:"Unable to save file \"%s\".",
