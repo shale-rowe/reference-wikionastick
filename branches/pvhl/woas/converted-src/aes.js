cf0820685ee6de7e4a9376d685baeb12b700db29
pvhl <pvhl@dadf181f-332c-0410-97d6-92020e263e48>
2010-03-27 08:06:42 +0000

    * Update converted code to 0.11.1

--- branches/pvhl/woas/converted-src/aes.js
+++ branches/pvhl/woas/converted-src/aes.js
@@ -1,18 +1,10 @@
-// Given the discussion below, aes2.js is a better choice than this file
-
 // AES encryption for StickWiki
 // adapted by legolas558
 // license: GNU/GPL
 // original code from http://home.versatel.nl/MAvanEverdingen/Code/
 // this is a javascript conversion of a C implementation by Mike Scott
 
-/*
-There are 287 matches for woas.AES in the current 0.11.0 code; 9 are outside aes.js
-With the wrapper there are just the API assignments at the end and external API calls.
-Code is therefore much easier to read and understand, I find.
-*/
-
-// namespace wrapper
+// namespace wrapper for woas.AES
 (function() {
 	
 var bData = null,
@@ -36,130 +28,44 @@ var bData = null,
 	aesFkey = null,
 	aesRkey = null;
 
-rotb = function(b,n){ return ( b<<n | b>>>( 8-n) ) & 0xFF; }
-rotw = function(w,n){ return ( w<<n | w>>>(32-n) ) & wMax; }
-getW = function(a,i){ return a[i]|a[i+1]<<8|a[i+2]<<16|a[i+3]<<24; }
-setW = function(a,i,w){ a.splice(i,4,w&0xFF,(w>>>8)&0xFF,(w>>>16)&0xFF,(w>>>24)&0xFF); }
-setWInv = function(a,i,w){ a.splice(i,4,(w>>>24)&0xFF,(w>>>16)&0xFF,(w>>>8)&0xFF,w&0xFF); }
-getB = function(x,n){ return (x>>>(n*8))&0xFF; }
+function rotb(b,n){ return ( b<<n | b>>>( 8-n) ) & 0xFF; }
+function rotw(w,n){ return ( w<<n | w>>>(32-n) ) & wMax; }
+function getW(a,i){ return a[i]|a[i+1]<<8|a[i+2]<<16|a[i+3]<<24; }
+function setW(a,i,w){ a.splice(i,4,w&0xFF,(w>>>8)&0xFF,(w>>>16)&0xFF,(w>>>24)&0xFF); }
+function setWInv(a,i,w){ a.splice(i,4,(w>>>24)&0xFF,(w>>>16)&0xFF,(w>>>8)&0xFF,w&0xFF); }
+function getB(x,n){ return (x>>>(n*8))&0xFF; }
 
-/*	var utf8sets = [0x800,0x10000,0x110000];
+function aesMult(x, y){ return (x&&y) ? aesPows[(aesLogs[x]+aesLogs[y])%255]:0; }
 
-	function unExpChar(c){
-	  return "unexpected character '"+String.fromCharCode(c)+"' (code 0x"+c.toString(16)+").";
-	}
-*/
-
-utf8Encrypt_s = function(sData) {
-	return unescape( encodeURIComponent( sData ) );
-}
-
-utf8Encrypt = function(sData){
-		return split_bytes(utf8Encrypt_s(sData));
-/*	  var k, i=0, z=sData.length;
-	  var bData = [];
-	  while (i<z) {
-	    c = sData.charCodeAt(i++);
-	    if (c<0x80){ bData.push(c); continue; }
-	    k=0; while(k<utf8sets.length && c>=utf8sets[k]) k++;
-	    if (k>=utf8sets.length) {
-			alert("UTF-8: "+unExpChar(c));
-			return null;
-		}
-		j=bData.length;
-	    for (var n=j+k+1;n>j;n--){ bData[n]=0x80|(c&0x3F); c>>>=6; }
-	    bData[j]=c+((0xFF<<(6-k))&0xFF);
-	    j += k+2;
-	  }
-	  return bData; */
-	}
-
-utf8Decrypt_s = function(sData) {
-		try {
-			return decodeURIComponent( escape( sData ) );
-		}
-		catch (e) {
-			log(e);	//log:1
-		}
-		return null;
-	}
-
-utf8Decrypt = function(bData){
-	return utf8Decrypt_s(merge_bytes(bData));
-}
-/*	  var z=bData.length;
-	  var c;
-	  var k, d = 0, i = 0;
-	  var sData = "";
-	  while (i<z) {
-	    c = bData[i++];
-	    k=0; while(c&0x80){ c=(c<<1)&0xFF; k++; }
-	    c >>= k;
-	    if (k==1||k>4) {
-	//		throw
-			log('UTF-8: invalid first byte');	// log:1
-			return null;
-		}
-	    for (var n=1;n<k;n++){
-	      d = bData[i++];
-	      if (d<0x80||d>0xBF) break;
-	      c=(c<<6)+(d&0x3F);
-	    }
-	    if ( (k==2&&c<0x80) || (k>2&&c<utf8sets[k-3]) ) {
-			log("UTF-8: invalid sequence");	// log:1
-			return null;
-		}
-	    sData+=String.fromCharCode(c);
-	  }
-	  return sData; */
-
-split_bytes = function(s) {
-	var l=s.length;
-	var arr=[];
-	for(var i=0;i<l;i++)
-		arr.push(s.charCodeAt(i));
-	return arr;
-}
-	
-merge_bytes = function(arr) {
-	var l=arr.length;
-	var s="";
-	for(var i=0;i<l;i++)
-		s+=String.fromCharCode(arr[i]);
-	return s;
-}
-
-aesMult = function(x, y){ return (x&&y) ? aesPows[(aesLogs[x]+aesLogs[y])%255]:0; }
-
-aesPackBlock = function() {
+function aesPackBlock() {
 	return [ getW(bData,aes_i), getW(bData,aes_i+4),
 			getW(bData,aes_i+8), getW(bData,aes_i+12) ];
 }
 
-aesUnpackBlock = function(packed){
+function aesUnpackBlock(packed){
   for ( var mj=0; mj<4; mj++,aes_i+=4) setW( bData, aes_i, packed[mj] );
 }
 
-aesXTime = function(p){
+function aesXTime(p){
   p <<= 1;
   return p&0x100 ? p^0x11B : p;
 }
 
-aesSubByte = function(w){
+function aesSubByte(w){
   return aesSBox[getB(w,0)] | aesSBox[getB(w,1)]<<8 | aesSBox[getB(w,2)]<<16 | aesSBox[getB(w,3)]<<24;
 }
 
-aesProduct = function(w1,w2){
+function aesProduct(w1,w2){
   return aesMult(getB(w1,0),getB(w2,0)) ^ aesMult(getB(w1,1),getB(w2,1))
        ^ aesMult(getB(w1,2),getB(w2,2)) ^ aesMult(getB(w1,3),getB(w2,3));
 }
 
-aesInvMixCol = function(x){
+function aesInvMixCol(x){
   return aesProduct(0x090d0b0e,x)     | aesProduct(0x0d0b0e09,x)<<8 |
          aesProduct(0x0b0e090d,x)<<16 | aesProduct(0x0e090d0b,x)<<24;
 }
 
-aesByteSub = function(x){
+function aesByteSub(x){
   var y=aesPows[255-aesLogs[x]];
   x=y;  x=rotb(x,1);
   y^=x; x=rotb(x,1);
@@ -168,7 +74,8 @@ aesByteSub = function(x){
   return x^y^0x63;
 }
 
-aesGenTables = function(){
+// Generate static tables
+(function(){
   var i,y;
   aesPows = [ 1,3 ];
   aesLogs = [ 0,0,null,1 ];
@@ -212,12 +119,9 @@ aesGenTables = function(){
     aesRi[m+2]=(4+j-3)%4;
   }
 
-}
+})();
 
-// these tables can be static
-aesGenTables();
-
-aesInit = function(){
+function aesInit(){
   key=key.slice(0,43);
   var i,k,m;
   var j = 0;
@@ -253,12 +157,11 @@ aesInit = function(){
   for (j=N-4;j<N;j++) aesRkey[j-N+4]=aesFkey[j];
 }
 
-aesClose = function(){
-//  aesFi=aesRi=aesPows=aesLogs=aesSBox=aesSBoxInv=aesRco=aesFtable=aesRtable=null;
+function aesClose(){
   aesFkey=aesRkey=null;
 }
 
-aesRounds = function( block, key, table, inc, box ){
+function aesRounds( block, key, table, inc, box ){
   var tmp = new Array( 4 );
   var i,j,m,r;
 
@@ -281,17 +184,17 @@ aesRounds = function( block, key, table, inc, box ){
   return tmp;
 }
 
-_encrypt = function(){
+function _encrypt(){
   aesUnpackBlock( aesRounds(aesPackBlock(), aesFkey, aesFtable, aesFi, aesSBox ) );
 }
 
-_decrypt = function(){
+function _decrypt(){
   aesUnpackBlock( aesRounds(aesPackBlock(), aesRkey, aesRtable, aesRi, aesSBoxInv ) );
 }
 
 // Blockcipher
 
-blcEncrypt = function(enc){
+function blcEncrypt(enc){
   if (tot==0){
 //    prgr = name;
     if (key.length<1) return;
@@ -310,7 +213,7 @@ blcEncrypt = function(enc){
   if (aes_i>=tot) aesClose();
 }
 
-blcDecrypt = function(dec){
+function blcDecrypt(dec){
 	// initialize length
   if (tot==0){
 //    prgr = name;
@@ -344,17 +247,21 @@ blcDecrypt = function(dec){
 }
 
 // sets global key to the utf-8 encoded key (byte array)
-setKey = function(sKey) {
+function setKey(sKey){
 	key = woas.utf8Encrypt(sKey);
 }
 
-clearKey = function() {
+function clearKey(){
 	key = [];
 }
 
+function isKeySet(){
+	return key.length > 0 ? true : false;
+}
+
 // returns an array of encrypted characters
-encrypt = function(raw_data) {
-	bData = utf8Encrypt(raw_data);
+function encrypt(raw_data){
+	bData = woas.utf8Encrypt(raw_data);
 	
 	aes_i=tot=0;
 	do{ blcEncrypt(_encrypt); } while (aes_i<tot);
@@ -365,7 +272,7 @@ encrypt = function(raw_data) {
 }
 
 // decrypts an array of encrypted characters
-decrypt = function(raw_data) {
+function decrypt(raw_data){
 	bData = raw_data;
 	
 	aes_i=tot=0;
@@ -374,15 +281,11 @@ decrypt = function(raw_data) {
 			return null;
 	} while (aes_i<tot);
 	
-	sData = utf8Decrypt(bData);
+	sData = woas.utf8Decrypt(bData);
 	bData = [];
 	var rv = sData;
 	sData = null;
 	return rv;
-}
-
-function isKeySet(){
-	return key.length > 0 ? true : false;
 }
 
 /*
@@ -390,74 +293,54 @@ Expose namespace; this allows:
 * simple calls in above code, so much more readable;
 * obvious definition of API;
 * for API to be overridden in a manner that allows simple chaining.
-This is all that is exposed.
+This is all of the above code that is exposed to other code.
 
 (Also, if namespace was "woas.crypto" would allow replacement plugins for different
-crypto methods to be called; a sort of Inversion Of Control injection. Pages could
-define their encryption method ("woas.page.encryption: aes256" ?) if desired, and
-the code just hooks into the defined api as here.
-
-Could work for other namespace APIs also.)
+crypto methods to be called; a sort of Inversion Of Control injection. Just write
+your new code to expose the same API. Pages could define their encryption method,
+e.g. ("woas.page.encryption: aes256" ?) if desired, and the code just hooks into
+the defined api as here. Would work for other namespace APIs also. Simple.)
 */
+
+// Expose namespace
 if (woas.AES === undefined) woas.AES = {};
 
 woas.AES.clearKey = clearKey;
 woas.AES.setKey = setKey;
+woas.AES.isKeySet = isKeySet; // keep key private to this code.
 woas.AES.encrypt = encrypt;
 woas.AES.decrypt = decrypt;
 
-/*
-I added this to stickwiki.js (replaced "key.length" with "isKeySet()"; 
-changes are trivial, but keep the key private, as it should be.
-(Also, just doing something like "woas.AES.key = key" doesn't work; would
-have to use woas.key above, I think. Haven't researched alternatives yet,
-but it is better to keep access to private data closed.)
-*/
-woas.AES.isKeySet = isKeySet;
+})(); // end of AES closure
 
-/*
-As the functions below are outside of this namespace they are probably in
-the wrong file or need to be renamed.
+// Other functions -- this suggests these might be in the wrong file :o)
 
-They could also be kept in this file but moved outside of the wrapper (with
-"woas." added to the calls inside the wrapper, of course). When learning new
-code it can be quite frustrating to find utility functions scattered around
-the files though (or is it just me?).
-*/
-woas.merge_bytes = merge_bytes;
-woas.split_bytes = split_bytes;
-woas.utf8Encrypt = utf8Encrypt;
-woas.utf8Decrypt = utf8Decrypt;
+woas.utf8Encrypt = function(sData) {
+	return woas.split_bytes( unescape( encodeURIComponent( sData ) ) );
+}
 
-// end namespace wrapper
-})();
+woas.utf8Decrypt = function(bData) {
+	try {
+		return decodeURIComponent( escape( woas.merge_bytes( bData ) ) );
+	}
+	catch (e) {
+		log(e);	//log:1
+	}
+	return null;
+}
 
-/*
-AFAIK, this wrapper behaviour is standard javascript functionality and always
-has been. I don't believe any browsers have issues with this from what I've read.
-
-Another way to do this is to define the public API functions within the code
-according to the namespace; e.g.:
-  woas.AES.clearKey = function(){key = [];}
-Now you don't have to manually expose the API, but the other wrapper advantages
-disappear somewhat.
-
-The wrapper can also have information passed in:
-  (function(info, obj){ ... })(my.info, {some:"data"});
-Such parameter data is part of the function closure and remains available to
-the code. jQuery uses this technique, among others. I used this in experiments
-to pass in the namespace, making it dynamic:
-  woas.myNs = {};
-  (function(ns){
-    ns.func1 = function() {}; //  and/or
-	ns.func2 = funct2; // for defining API as before.
-  })(woas.myNs);
-Useful in terms of module reuse perhaps but I haven't experimented much with
-this so don't know strengths/weaknesses. Also can't remember the rules for
-how live the passed in data remains, but there is lots of info around on this.
-
-All these comments about modules/closures should be removed. Just FYI.
-
-A better modularization of AES, as suggested above, is in aes2.js
-
-*/
+woas.split_bytes = function(s) {
+	var l=s.length;
+	var arr=[];
+	for(var i=0;i<l;i++)
+		arr.push(s.charCodeAt(i));
+	return arr;
+}
+	
+woas.merge_bytes = function(arr) {
+	var l=arr.length;
+	var s="";
+	for(var i=0;i<l;i++)
+		s+=String.fromCharCode(arr[i]);
+	return s;
+}
--- branches/pvhl/woas/converted-src/i18n.js
+++ branches/pvhl/woas/converted-src/i18n.js
@@ -23,6 +23,7 @@ woas.i18n = {
 	DELETE_PAGE_PROMPT: "Delete page:",
 	DISPLAY_FULL_FILE: "Display full file",
 	DUP_NS_ERROR: "Cannot duplicate into File:: or Image:: namespace!",
+	EDITING:"Editing %s",
 	EMPTY_TITLE: "Pages with empty title are not allowed",
 	ERR_MARKER: "%s marker not found!",
 	ERR_NO_PWD: "No password set for decryption of page '%s'\nPlease click the key icon and enter a password",
