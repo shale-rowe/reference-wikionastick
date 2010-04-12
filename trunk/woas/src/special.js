8fe84a083004ea562249e9a35d6e0516ee275361
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-04-12 07:47:17 +0000

    * moved folding code to special.js * fixed issue 2985218 tagged listings broken (reported by littlegirl)

--- trunk/woas/src/special.js
+++ trunk/woas/src/special.js
@@ -178,17 +178,16 @@ woas.special_tagged = function() {
          return -1;
       return 0;
     });
-	var pg=[], obj, it;
+	var obj, it, folds={"[pages]":[]};
 	for(j=0,l=utags.length;j<l;j++) {
 		obj = tags_tree[utags[j]].sort();
-		pg.push("\n== [[Tagged::"+utags[j]+"]]");
+		if (typeof folds[utags[j]] == "undefined")
+			folds[utags[j]] = {"[pages]":[]};
 		for(i=0,it=obj.length;i<it;i++) {
-			pg.push(obj[i]);
+			folds["Tagged::"+utags[j]]["[pages]"].push(obj[i]);
 		}
 	}
-	if (pg.length)
-		return this._join_list(pg);
-	return '/No tagged pages/';
+	return woas.ns_parse_tree(folds);
 };
 
 woas.special_untagged = function() {
@@ -332,4 +331,85 @@ woas.special_recent_changes = function() {
 	return this._simple_join_list(pg);
 };
 
-// End of special pages' code
+// joins a list of pages
+woas._join_list = function(arr) {
+	if (!arr.length)
+		return "";
+	// copy the array to currently selected pages
+	result_pages = arr.slice(0);
+	//return "* [["+arr.sort().join("]]\n* [[")+"]]";
+	// (1) create a recursable tree of namespaces
+	var ns,output={"s":"","fold_no":0},folds={"[pages]":[]},i,ni,nt,key;
+	for(i=0,it=arr.length;i<it;++i) {
+		ns = arr[i].split("::");
+		// remove first entry if empty
+		if (ns.length>1) {
+			if (ns[0].length == 0) {
+				ns.shift();
+				ns[0] = "::"+ns[0];
+			}
+			// recurse all namespaces found in page title
+			this.ns_recurse(ns, folds, "");
+		} else // <= 1
+			folds["[pages]"].push(arr[i]);
+	}
+	// (2) output the tree
+	this.ns_recurse_parse(folds, output, "", 0);
+	return output.s;
+};
+
+woas.ns_recurse = function(ns_arr, folds, prev_ns) {
+	var ns = prev_ns+ns_arr.shift()+"::", item, left=ns_arr.length;;
+	if (typeof folds[ns] == "undefined") {
+		// last item, build the array
+		if (left == 1) {
+			folds[ns] = {"[pages]": [ns+ns_arr[0]] };
+			return;
+		}
+		// namespace, create object
+		folds[ns] = {"[pages]":[]};
+	} else { // object already exists, add only leaves
+		if (left == 1) {
+			folds[ns]["[pages]"].push(ns+ns_arr[0]);
+			return;
+		}
+	}
+	if (left > 1)
+		this.ns_recurse(ns_arr, folds, ns);
+};
+
+woas.ns_recurse_parse = function(folds, output, prev_ns, recursion) {
+	var i,it=folds["[pages]"].length,fold_id;
+	if (it != 0) {
+		fold_id = "fold"+output.fold_no++;
+		++recursion;
+		// disable folding for pages outside namespaces
+		if (prev_ns.length) {
+			output.s += "=".repeat(recursion)+" [[Javascript::$.toggle('"+fold_id+"')|"+prev_ns+"]]";
+			output.s += " [["+prev_ns+"|"+String.fromCharCode(0x21DD)+"]] ("+it+" pages)\n";
+			output.s += "<div style=\"visibility: visible\" id=\""+fold_id+"\">\n";
+		}
+		for(i=0;i<it;++i) {
+			output.s += "* [["+folds["[pages]"][i]+"]]\n";
+		}
+		if (prev_ns.length)
+			output.s += "</div>\n";
+	}
+	for(i in folds) {
+		if (i != "[pages]")
+			this.ns_recurse_parse(folds[i], output, prev_ns+i, recursion);
+	}
+};
+
+woas._simple_join_list = function(arr, sorted) {
+	if (sorted)
+		arr = arr.sort();
+	// a newline is added here
+	return arr.join("\n")+"\n";
+};
+
+woas.ns_parse_tree = function(folds) {
+	var output={"s":""};
+	this.ns_recurse_parse(folds, output, "", 0);
+	return output.s;
+};
