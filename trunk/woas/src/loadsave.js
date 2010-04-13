55de080c08d99651279e95d4cd78c5d686954d67
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-04-13 08:05:51 +0000

    * do not store current page and backstack if remember history option is disabled * renamed open last page option to remember history and current page option

--- trunk/woas/src/loadsave.js
+++ trunk/woas/src/loadsave.js
@@ -445,9 +445,9 @@ woas._save_to_file = function(full) {
 	// setup the page to be opened on next start
 	var safe_current;
 	if (this.config.open_last_page) {
-		if (!this.page_exists(current)) {
+		if (!this.page_exists(current))
 			safe_current = this.config.main_page;
-		} else safe_current = current;
+		else safe_current = current;
 	} else
 		safe_current = this.config.main_page;
 	
@@ -473,7 +473,7 @@ woas._save_to_file = function(full) {
 	
 	computed_js += "\nvar current = '" + this.js_encode(safe_current)+"';\n\n";
 	
-	computed_js += "var backstack = [\n" + printout_arr(backstack, false) + "];\n\n";
+	computed_js += "var backstack = [\n" + printout_arr(this.config.open_last_page ? backstack : [], false) + "];\n\n";
 	
 	// in native WSIF mode we will save empty arrays
 
