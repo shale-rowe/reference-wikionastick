b51cf64c5795b98641fca5fa3fc91f8390661360
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-04-30 07:57:47 +0000

    * finished implementation of macro parameters

--- woas/src/macro.js
+++ woas/src/macro.js
@@ -1,5 +1,5 @@
-
 var reMacroDef = /^(%?[A-Za-z0-9_\.]+)\s*(\(.*?\))?\s*:([\s\S]*)$/;
+
 // macro syntax plugin code adapted from FBNil's implementation
 woas.macro_parser = function(text){
 	// macro object
@@ -33,28 +33,30 @@ woas.macro_parser = function(text){
 		var fi = this.macro_parser.macro_names.indexOf(fn);
 		if (fi !== -1) {
 			macro.text = M[3];
-			this.macro_parser.macro_functions[fi](macro, this.macro_parser.pass_args(M[2]));
+			// if we have no parameters, direct call function
+			var pl;
+			if (typeof M[2] == "undefined")
+				pl = 0;
+			else pl = M[2].length;
+			if (pl === 0)
+				(this.macro_parser.macro_functions[fi])(macro);
+			else {
+				// inline insertion of parameters
+				var code = "(woas.macro_parser.macro_functions["+fi+"])"+
+							"(macro,"+M[2].substr(1,pl-2)+");";
+				try {
+					eval(code);
+				}
+				catch(e) {
+					woas.log("Error during macro execution: "+e);
+				}
+			}
 		} else {
 			log("Undefined macro "+fn);	//log:1
 			macro.text = woas._make_preformatted(macro.text, "color:red");
 		}
 	}
 	return macro;
-};
-
-var reFindArgVal = /("|')[^\1]*\1\s*,/g,
-	reFindArgValLast = /("|')[^\1]*\1\s*/g;
-woas.macro_parser.pass_args = function(flat_args) {
-	var params = [];
-	reFindArgVal.lastIndex = 0;
-	flat_args.replace(reFindArgVal, function(str) {
-		params.push(str);
-	});
-	reFindArgValLast.lastIndex = reFindArgVal.lastIndex;
-	flat_args.replace(reFindArgValLast, function(str) {
-		params.push(str);
-	});
-	return params;
 };
 
 // this is the function to be called to register a  macro
@@ -99,8 +101,9 @@ woas.macro_parser.default_macros = {
 woas.macro_parser.macro_names = ["woas.include"];
 woas.macro_parser.macro_functions = [woas.macro_parser.default_macros.include];
 
-var reFindArgDef = /[a-z0-9_]+\s*,/gi,
-	reFindArgDefLast = /[a-z0-9_]+\s*$/gi;
+// a reduced charset for javascript argument names
+var reFindArgDef = /([a-z0-9_]+)\s*,/gi,
+	reFindArgDefLast = /([a-z0-9_]+)\s*$/gi;
 woas.macro_parser.create = function(fn_name, fn_params, fn_code) {
 	// duplicated from register function
 	if (woas.macro_parser.macro_names.indexOf(fn_name) != -1) {
@@ -110,21 +113,27 @@ woas.macro_parser.create = function(fn_name, fn_params, fn_code) {
 	// prepare for passing other params
 	// parameter definitions can be very limited in charset
 	var real_params=[], other_params = "";
-	reFindArgDef.lastIndex = 0;
-	fn_params.replace(reFindArgDef, function(str) {
-		real_params.push(str);
-	});
-	reFindArgDefLast.lastIndex = reFindArgDef.lastIndex;
-	fn_params.replace(reFindArgDefLast, function(str) {
-		real_params.push(str);
-	});
+	if (fn_params.length) {
+		// remove parenthesis
+		fn_params = fn_params.substr(1, fn_params.length-2);
+		reFindArgDef.lastIndex = 0;
+		fn_params.replace(reFindArgDef, function(str, pname) {
+			real_params.push(pname);
+		});
+		
+		reFindArgDefLast.lastIndex = reFindArgDef.lastIndex;
+		fn_params.replace(reFindArgDefLast, function(str, pname) {
+			real_params.push(pname);
+		});
+	}
 
 	if (real_params.length)
 		other_params = ","+real_params.join(",");
 	else other_params = "";
 	var obj = null;
 	try {
-		eval("obj = function "+fn_name+"(macro"+other_params+") {\n"+fn_code+"\n}");
+		var snippet = "obj = function "+fn_name+"(macro"+other_params+") {\n"+fn_code+"\n}";
+		eval(snippet);
 	}
 	catch (e) {
 		log("cannot define function "+fn_name+": "+e); //log:1
