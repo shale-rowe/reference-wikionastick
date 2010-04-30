9f3718e2a82b95171946282e571292166bbc7d4e
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-04-30 08:24:12 +0000

    * implementation of shjs plugin

--- woas/plugins/syntaxHL.js
+++ woas/plugins/syntaxHL.js
@@ -1,6 +1,6 @@
 /* shjs binding for WoaS
    @author legolas558
-   @version 0.1.0
+   @version 0.1.5
    @license GPLv2
 
    works with shjs 0.6
@@ -10,18 +10,58 @@
 woas.custom.shjs = {
 	
 	// we take an unique id for our job
+	is_loaded: false,
 	_uid: _random_string(8),
 	
 	init: function() {
-		
+		if (!this.is_loaded)
+			this.is_loaded = woas.dom.add_script("lib", "shjs", "plugins/shjs/sh_main.js", true);
+		return this.is_loaded;
 	},
 	
-	_macro_hook: function(macro) {
-		
+	// this was adapted from shjs' sh_highlightDocument
+	_highlight_element: function(element, htmlClasses) {
+		for (var j = 0; j < htmlClasses.length; j++) {
+			var htmlClass = htmlClasses[j].toLowerCase();
+			if (htmlClass === 'sh_sourcecode') {
+				continue;
+			}
+			if (htmlClass.substr(0, 3) === 'sh_') {
+				var language = htmlClass.substring(3);
+				if (language in sh_languages) {
+					sh_highlightElement(element, sh_languages[language]);
+/*				} else if (typeof(prefix) === 'string' && typeof(suffix) === 'string') {
+					sh_load(language, element, prefix, suffix); */
+				} else {
+					woas.log('Element has class="' + htmlClass + '", but no such language exists');
+				}
+				break;
+			}
+		} // for htmlClasses
+	},
+	
+	_macro_hook: function(macro, classes) {
+		// rebuild the classes string as a simple JSON array
+		var classes = classes.split(' '), classes_v="[", cl = classes.length;
+		if (cl) {
+			for (var i = 0; i < cl-1; i++) {
+				if (classes[i].length > 0) {
+					classes_v += "'"+woas.js_encode(woas.trim(classes[i]))+"',";
+				}
+			}
+			// add the last one
+			classes_v += "'"+woas.js_encode(woas.trim(classes[cl-1]))+"'";
+		}
+		classes_v += "]";
+
+		macro.text = "<"+"div id=\"woas_shjs_"+this._uid+"\">"+macro.text+"<"+"/div>"+
+					"<"+"script type=\"text/javascript\">"+
+					"_highlight_element($('woas_shjs_"+this._uid+"',"+classes_v+");"+
+					"<"+"/script>";
+		macro.reprocess = true;
 	}
 	
 };
-
 
 // initialize the library
 woas.custom.shjs.init();
