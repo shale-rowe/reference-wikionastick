5e887c3394f5f3d759ce6d1da987576672733d07
littlegirl <littlegirl@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-01 03:55:44 +0000

    * Added the gfx subdirectory. * Added the other subdirectory. * Added the other/shortcut.js file. * Added the boas/plugins subdirectory. * Added the boas/plugins/jsMath.js file. * Added the boas/plugins/syntaxHL.js file. * Added the boas/plugins/jsMath subdirectory. * Added the boas/plugins/jsMath/GETME file. * Added the boas/plugins/shjs subdirectory. * Added the boas/plugins/shjs/GETME file. * Added the tests subdirectory. * Added the tests/aes_tests.htm file. * Added the tools subdirectory.

---
+++ branches/littlegirl/bookonastick/boas/plugins/syntaxHL.js
@@ -0,0 +1,128 @@
+/* shjs binding for WoaS
+   @author legolas558
+   @version 0.1.11
+   @license GPLv2
+
+   works with shjs 0.6
+   be sure that you have downloaded and extracted shjs in shjs directory
+*/
+
+woas.custom.shjs = {
+	
+	// we take an unique id for our job
+	is_loaded: false,
+	_uid: _random_string(8),
+	_block: 0,			// current block
+	_original_hook: null,
+	
+	init: function() {
+		if (!this.is_loaded) {
+			this.is_loaded = woas.dom.add_script("lib", "shjs", "plugins/shjs/sh_main.min.js", true) &&
+							woas.dom.add_css("shjs_style", "plugins/shjs/sh_style.min.css", true);
+			// subclass post-load hook
+			woas.custom.shjs._original_hook = woas.parser.post_load;
+			woas.parser.post_load = function() {
+//				if (woas.custom.shjs._block !== 0)
+//					woas.custom.shjs._render_all_thread();
+				// launch original handler
+				woas.custom.shjs._original_hook();
+			};
+		}
+		return this.is_loaded;
+	},
+	
+	_render_all_thread: function() {
+		// repeat if library is not yet available
+		if (typeof sh_languages == "undefined") {
+			woas.log("Waiting for shjs library...");
+			setTimeout("woas.custom.shjs._render_all_thread();", 600);
+			return;
+		}
+		// render everything
+		woas.custom.shjs._render_all();
+		// finish
+	},
+	
+	_render_all: function() {
+		// render all blocks by clicking their button
+		for(var i=0;i<woas.custom.shjs._block;++i) {
+			$("shjs_postr_btn_"+woas.custom.shjs._uid+"_"+i).click();
+		}
+		// clear
+		woas.custom.shjs._block = 0;
+	},
+	
+	// this was adapted from shjs' sh_highlightDocument
+	_highlight_element: function(element, languages) {
+		for (var j = 0; j < languages.length; j++) {
+			if (languages[j] in sh_languages) {
+				sh_highlightElement(element, sh_languages[languages[j]]);
+			} else {
+				woas.log("Cannot render language "+languages[j]);
+			}
+			break;
+		}
+	},
+
+	// used for post-rendering after library was loaded
+	post_render: function(i, languages) {
+		$.hide("shjs_postr_btn_"+this._uid+"_"+i);
+		var elem = $("woas_shjs_"+this._uid+"_"+i);
+		this._highlight_element(elem, languages);
+		return;
+	},
+	
+	_macro_hook: function(macro, classes) {
+		// shjs library not yet loaded, go into pre-render mode
+		var pre_render = (typeof sh_languages == "undefined");
+
+		// rebuild the classes string as a simple JSON array
+		var classes_arr = classes.split(' '), classes_v="[", cl = classes_arr.length;
+		if (cl) {
+			var language;
+			for (var i = 0; i < cl; i++) {
+				if (classes_arr[i].length > 0) {
+					language = woas.trim(classes_arr[i]).toLowerCase();
+					if (language.substr(0,3) !== "sh_")
+						continue;
+					language = language.substr(3);
+					// skip this one because already rendered
+					if (language === "sourcecode")
+						continue;
+					classes_v += "'"+woas.js_encode(language)+"',";
+					if (pre_render || !(language in sh_languages)) {
+						// load this library
+						woas.dom.add_script("lib", "shjs_"+language, "plugins/shjs/lang/sh_"+language+".min.js", true);
+					}
+				}
+			}
+			// remove final comma
+			classes_v = classes_v.substr(0, classes_v.length-1);
+		}
+		classes_v += "]";
+
+		macro.text = "<"+"pre id=\"woas_shjs_"+woas.custom.shjs._uid+"_"+woas.custom.shjs._block+"\" class=\""+classes+"\">"+macro.text+"<"+"/pre>";
+		if (pre_render) {
+			macro.text += "<"+"input id=\"shjs_postr_btn_"+woas.custom.shjs._uid+
+						"_"+woas.custom.shjs._block+"\" type=\"button\" value=\"Render\" onclick=\"woas.custom.shjs.post_render("+woas.custom.shjs._block+","+classes_v+");\" /"+">";
+		} else {
+			// inline script for highlighting
+			// we access the script_extension private array here -- UNSUPPORTED!
+			woas.parser.script_extension.push(
+					"woas.custom.shjs._highlight_element($('woas_shjs_"+woas.custom.shjs._uid+"_"+woas.custom.shjs._block+"'),"+classes_v+");"
+			);
+					
+		}
+		macro.reprocess = true;
+		++woas.custom.shjs._block;
+		// reset block counter
+		if (!pre_render)	this._block = 0;
+	}
+	
+};
+
+// initialize the library
+woas.custom.shjs.init();
+
+// register the macro
+woas.macro_parser.register('woas.shjs', woas.custom.shjs._macro_hook);
