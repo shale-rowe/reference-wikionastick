2d6b61d4e85ba9f078e4565ade902e30f62af376
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-01 20:15:46 +0000

    * added more description for woas._native_wsif_load function * save WoaS after importing a WSIF file

--- woas/src/ui.js
+++ woas/src/ui.js
@@ -498,6 +498,7 @@ function ff3_getPath(fileBrowser) {
 	return fileName;
 }
 
+// Special::ImportWSIF
 function import_wiki_wsif() {
 	if (!woas.config.permit_edits) {
 		this.alert(woas.i18n.READ_ONLY);
@@ -509,7 +510,7 @@ function import_wiki_wsif() {
 	_wsif_js_sec.comment_js = $("woas_cb_import_comment_js").checked;
 	_wsif_js_sec.comment_macros = $("woas_cb_import_comment_macros").checked;
 	_wsif_js_sec.woas_ns = $("woas_cb_import_woas_ns").checked;
-	// automatically retrieve the filename (calls load_file())
+	// automatically retrieve the filename (will call load_file())
 	done = woas._native_wsif_load(null, $("woas_cb_import_overwrite").checked, true, false,
 			_import_wsif_pre_hook);
 	if (done === false && (woas.wsif.emsg !== null))
@@ -521,6 +522,8 @@ function import_wiki_wsif() {
 			done = String(done)+"/"+woas.wsif.expected_pages;
 		woas.alert(woas.i18n.IMPORT_OK.sprintf(done, woas.wsif.system_pages));
 		woas.refresh_menu_area();
+		// now proceed to actual saving
+		woas.commit(woas.wsif.imported);
 	}
 	return done;
 }
