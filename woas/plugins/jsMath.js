3bcefe2ea7b700bf872e971587483d5bb6be20f0
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-01 18:14:11 +0000

    * seeking for a working mode for jsMath

--- woas/plugins/jsMath.js
+++ woas/plugins/jsMath.js
@@ -16,22 +16,24 @@ jsMath = {
 		// override font messages
 		Message : function(msg) {woas.log(woas.xhtml_to_text(msg).replace(/\n\n/g, "\n"));}
 	},
+	Setup: {UserEvent: {
+      onload: function () {
+         woas.custom.jsMath._after_lib_loaded();
+       }
+    }},
+
 	noGoGlobal:1,
 	noChangeGlobal:1,
 	noShowGlobal:1
 };
 
 woas.custom.jsMath = {
-	is_loaded: false,
 	init : function() {
-		if (!this.is_loaded)
-        // setup jsMath config object and
-        // load libraries after defining global jsMath
-		this.is_loaded =  woas.dom.add_script("lib", "jsMath0", "plugins/jsMath/plugins/noImageFonts.js", true,
-								woas.custom.jsMath.render_all) &&
-						woas.dom.add_script("lib", "jsMath1", "plugins/jsMath/jsMath.js", true,
-								woas.custom.jsMath.render_all);
-		return this.is_loaded;
+		// nothing here
+	},
+	
+	_after_lib_loaded: function() {
+		alert("library finished loading");
 	},
 	
 	_block: 0,		// number of pre tags to render after library finishes loading
@@ -61,13 +63,13 @@ woas.custom.jsMath = {
 	_macro_hook : function(macro) {
 		 // quit if libraries have not yet been loaded and
 		 // increase counter for post-rendering
-/*		 if (typeof jsMath.Process == "undefined") {
+		 if (typeof jsMath.Process == "undefined") {
 			macro.text = "<"+"div id=\"jsmath_postrender_"+woas.custom.jsMath._block+"\">"+macro.text+"<"+"/div>"+
 						"<"+"input id=\"jsmath_postr_btn_"+woas.custom.jsMath._block+
 						"\" type=\"button\" value=\"Render\" onclick=\"woas.custom.jsMath.post_render("+woas.custom.jsMath._block+");\" /"+">";
 			++this._block;
 			return;
-		 } */
+		 }
 // 		this._block = 0;
 		jsMath.Init();
 		macro.text = jsMath.Translate.Parse('T', macro.text);
@@ -76,7 +78,7 @@ woas.custom.jsMath = {
 };
 
 // initialize the library
-woas.custom.jsMath.init();
+//woas.custom.jsMath.init();
 
 // register the macro
 woas.macro_parser.register('woas.jsmath', woas.custom.jsMath._macro_hook);
