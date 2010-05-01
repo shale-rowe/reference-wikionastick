fa7344b080f4a3670a53ff6478eea2ca8184a04a
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-01 15:19:03 +0000

    * add proper initialization in separate code block * do not init inside macro (not necessary)

--- trunk/woas/plugins/jsMath.js
+++ trunk/woas/plugins/jsMath.js
@@ -7,35 +7,51 @@
    be sure that you have downloaded and extracted jsMath in jsMath directory
 */
 
+// this is necessary before loading any jsMath library
+jsMath = {
+	Controls: {
+		cookie: {scale: 133, global: 'never', button:0}
+	},
+	Font: {
+		// override font messages
+		Message : function(msg) {woas.log(woas.xhtml_to_text(msg).replace(/\n\n/g, "\n"));}
+	},
+	noGoGlobal:1,
+	noChangeGlobal:1,
+	noShowGlobal:1
+};
+
 woas.custom.jsMath = {
 	is_loaded: false,
-	_block: 0,		// number of divs to render after library finishes loading
+	_block: 0,		// number of pre tags to render after library finishes loading
 	init : function() {
 		if (!this.is_loaded)
         // setup jsMath config object and
         // load libraries after defining global jsMath
-		this.is_loaded = woas.dom.add_script("lib", "jsMath0", "\
-         jsMath = {\
-            Controls: {\
-              cookie: {scale: 133, global: 'never', button:0}\
-            },\
-            Font: {\
-		Message : function(msg) {woas.log(woas.xhtml_to_text(msg).replace(/\\n\\n/g, \"\\n\"));}\
-	    },\
-         noGoGlobal:1,\
-         noChangeGlobal:1,\
-         noShowGlobal:1\
-         };", false) &&
-			 woas.dom.add_script("lib", "jsMath1", "plugins/jsMath/plugins/noImageFonts.js", true) &&
-			 woas.dom.add_script("lib", "jsMath2", "plugins/jsMath/jsMath.js", true);
-	     return this.is_loaded;
+		this.is_loaded =  woas.dom.add_script("lib", "jsMath0", "plugins/jsMath/plugins/noImageFonts.js", true,
+								woas.custom.jsMath.render_all) &&
+						woas.dom.add_script("lib", "jsMath1", "plugins/jsMath/jsMath.js", true,
+								woas.custom.jsMath.render_all);
+		return this.is_loaded;
 	},
 	
-	//             UserEvent: { onload: woas.custom.jsMath.post_render },
+	_called: 0,
+	_rendering: false,
+	render_all: function() {
+		if (woas.custom.jsMath._rendering) return;
+		if (++woas.custom.jsMath._called == 2) {
+			woas.custom.jsMath._rendering = true;
+			jsMath.Init();
+			for(var i=0;i<woas.custom.jsMath._block;++i) {
+				woas.custom.jsMath.post_render(i);
+			}
+			woas.custom.jsMath._block = 0;
+		}
+	},
 	
 	// used for post-rendering after library was loaded
 	post_render: function(i) {
-		jsMath.Init();
+//		jsMath.Init();
 		var elem = $("jsmath_postrender_"+i);
 		woas.setHTML(elem, jsMath.Translate.Parse('T', woas.getHTML(elem)));
 		$.hide("jsmath_postr_btn_"+i);
