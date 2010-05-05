58f24795d6dcc6e3aff00f2974cf555d822b3db8
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-05 09:49:44 +0000

    * dropped support for all versions older than 0.9.6B * safer&cleaner config import

--- woas/src/import.js
+++ woas/src/import.js
@@ -1,3 +1,6 @@
+// skeleton for the 'importer' module
+woas.importer = {
+};
 
 // regular expressions used to not mess with title/content strings
 var reRequote = new RegExp(":-"+parse_marker, "g"),
@@ -20,7 +23,37 @@ function _new_syntax_patch(text) {
 	return text;
 }
 
-var reValidImage = /^data:\s*[^;]*;\s*base64,\s*/;
+/* NOTES ABOUT BACKWARD IMPORT SUPPORT
+		0.12.0:
+			* introduced some config options: new_tables_syntax, store_mts, folding_style
+			* introduced plugins (which deprecate WoaS::Bootscript)
+			* dropping support for all WoaS before 0.9.6B
+		0.11.2:
+			* introduced parsing mechanism which does not mess with var declarations inside JavaScript strings
+			* introduced WoaS::Hotkeys, WoaS::CSS::Core, WoaS::CSS::Custom
+			* introduced woas.config.safe_mode option
+			* introduced woas.config.wsif_ds* options
+		0.10.7:
+			* introduced WoaS::Plugins and changed WoaS::Bootscript page type from embedded to normal
+		0.10.0:
+			* introduced page_mts for global page modified timestamp
+		0.9.7:
+			* introduced WoaS::Aliases
+		0.9.6:
+			* Special::Bootscript -> WoaS::Bootscript
+		0.9.5D (not released)
+			* Javascript:: reserved namespace
+			* some Special:: pages no more work
+		0.9.5B
+			* object orientation of code
+			* server_mode disappears
+		0.9.4B
+			* introduced Special::Bootscript
+*/
+
+
+var reValidImage = /^data:\s*[^;]*;\s*base64,\s*/,
+	reOldStyleBlock = new RegExp("<"+"style\\s.*?type=\"?text\\/css\"?[^>]*>((\\n|.)*?)<"+"\\/style>", "i");
 woas.import_wiki = function() {
 
 	// function used to collect variables
@@ -94,24 +127,6 @@ woas.import_wiki = function() {
 	ver_str = ver_str[1];
 	log("Version string: "+ver_str);	// log:1
 	switch (ver_str) {
-		case "0.9B":
-		case "0.9":
-			old_version = 9;
-		break;
-		case "0.9.2B":
-			old_version = 92;
-		break;
-		case "0.9.3B":
-			old_version = 93;
-		break;
-		case "0.9.4B":
-			old_version = 94;
-		break;
-		case "0.9.5B":
-		case "0.9.5C":
-//		case "0.9.5D": // development only
-			old_version = 95;
-			break;
 		case "0.9.6B":
 //		case "0.9.6C": // development only
 			old_version = 96;
@@ -141,7 +156,6 @@ woas.import_wiki = function() {
 
 	// import the variables
 	var new_main_page = this.config.main_page,
-		old_block_edits = !this.config.permit_edits,
 		page_contents = [],
 		old_page_attrs = [],
 		old_page_mts = [],
@@ -149,46 +163,8 @@ woas.import_wiki = function() {
 		i, il, pi,
 		imported_css = null,
 		// used during import from older versions
-		old_cfg = {"debug_mode":this.config.debug_mode,
-				"wsif_ds":this.config.wsif_ds,
-				"wsif_ds_multi":this.config.wsif_ds_multi,
-				"wsif_ds_lock":this.config.wsif_ds_lock,
-				"safe_mode":this.config.safe_mode,
-				"wsif_author":this.config.wsif_author,
-				"main_page":this.config.main_page,
-				"new_tables_syntax":this.new_tables_syntax,
-				"store_mts":this.config.store_mts,
-				"folding_style":this.config.folding_style
-				};
+		old_cfg = $.clone(this.config);
 
-		/* NOTES ABOUT OLD VERSIONS
-		0.12.0:
-			* introduced some config options: new_tables_syntax, store_mts
-			* introduced plugins (which deprecate WoaS::Bootscript)
-		0.11.2:
-			* introduced parsing mechanism which does not mess with var declarations inside JavaScript strings
-			* introduced WoaS::Hotkeys, WoaS::CSS::Core, WoaS::CSS::Custom
-			* introduced woas.config.safe_mode option
-			* introduced woas.config.wsif_ds* options
-		0.10.7:
-			* introduced WoaS::Plugins and changed WoaS::Bootscript page type from embedded to normal
-		0.10.0:
-			* introduced page_mts for global page modified timestamp
-		0.9.7:
-			* introduced WoaS::Aliases
-		0.9.6:
-			* Special::Bootscript -> WoaS::Bootscript
-		0.9.5D (not released)
-			* Javascript:: reserved namespace
-			* some Special:: pages no more work
-		0.9.5B
-			* object orientation of code
-			* server_mode disappears
-		0.9.4B
-			* introduced Special::Bootscript
-		*/
-
-	// we are importing a v0.9.x Beta
 	// locate the random marker
 	var old_marker = ct.match(/\nvar __marker = "([A-Za-z\-\d]+)";(\r\n|\n)/);
 	if (!old_marker) {
@@ -200,7 +176,7 @@ woas.import_wiki = function() {
 
 	// import the CSS head tag in versions before 0.11.2
 	if (import_css && (old_version < 112)) {
-		ct.replace(/<style\s.*?type="?text\/css"?[^>]*>((\n|.)*?)<\/style>/i, function (str, $1) {
+		ct.replace(reOldStyleBlock, function (str, $1) {
 			imported_css = $1;
 		});
 		if (imported_css !== null) {
@@ -212,121 +188,64 @@ woas.import_wiki = function() {
 	var data = this._extract_src_data(old_marker, ct, true, new_main_page, true),
 		collected = [];
 		
-	// for versions before v0.9.2B
-	if (old_version < 92) {
-		collected = get_import_vars(data);
-		data = ct = null;
+	// some GC help: we no more need the big content variable
+	ct = null;		
+	collected = get_import_vars(data, ['woas', '__marker', 'version', '__config']);
 
-		var has_last_page_flag = (collected.defs.length==14) ? 1 : 0;
-		if (!has_last_page_flag && (collected.defs.length!=13)) {
-			this.alert(this.i18n.INVALID_DATA);
-			fail=true;
-			break;
+	if (old_version > 108)
+		new_main_page = collected.main_page;
+	if (import_content) {
+		if (old_version > 97)
+			old_page_mts = collected.page_mts;
+		// copy the big arrays
+		page_names = collected.page_titles;
+		page_contents = collected.pages;
+		old_page_attrs = collected.page_attrs;
+	} // do not import content pages
+
+	var cfgStartMarker = 'woas["'+'config"] = {',
+		// grab the woas config definition
+		cfg_start = data.indexOf(cfgStartMarker),
+		cfg_found = false;
+	
+	if (cfg_start !== -1) {
+		var cfg_end = data.indexOf('}', cfg_start+cfgStartMarker.length);
+		if (cfg_end !== -1) {
+			woas.config = woas.eval(data.substring(cfg_start+cfgStartMarker.length-1, cfg_end+1), true);
+			cfg_found = !woas.eval_failed;
 		}
-			
-		// set collected config options
-		old_block_edits = !collected[collected.defs[2]];
-		this.config.dblclick_edit = collected[collected.defs[3]];
-		this.config.save_on_quit = collected[collected.defs[4]];
-		if (has_last_page_flag)
-			this.config.nav_history = collected[collected.defs[5]];
-		this.config.allow_diff = collected[collected.defs[5+has_last_page_flag]];
-		this.config.key_cache = collected[collected.defs[6+has_last_page_flag]];
-		// the gathered data
-		new_main_page = collected[collected.defs[8+has_last_page_flag]];
-		page_names = collected[collected.defs[10+has_last_page_flag]];
-		old_page_attrs = collected[collected.defs[11+has_last_page_flag]];
-		page_contents = collected[collected.defs[12+has_last_page_flag]];
-		
-	} else {	// we are importing from v0.9.2 and above which has a config object for all the config flags
-			// some GC help: we no more need the big content variable
-			ct = null;		
-			// old-style import for content, skipping the main woas object and the marker
-			// shared with v0.9.5B
-			collected = get_import_vars(data, ['woas', '__marker', 'version', '__config']);
-
-			//0:sw_import_current,1:sw_import_main_page,
-			//2:sw_import_backstack,3:sw_import_page_titles,4:sw_import_page_attrs,5:sw_import_pages
-			// from 0.10.0: sw_page_mts is before sw_import_pages
-			// from 0.10.9: main_page is inside woas.config
-			if (old_version <= 108)
-				new_main_page = collected[collected.defs[1]];
-			if (import_content) {
-				// offset for missing main_page var
-				var ofs_mp = (old_version <= 108) ? 0 : -1;
-				if (old_version <= 97)
-					page_contents = collected[collected.defs[5+ofs_mp]];
-				else {
-					old_page_mts = collected[collected.defs[5+ofs_mp]];
-					page_contents = collected[collected.defs[6+ofs_mp]];
-				}
-				page_names = collected[collected.defs[3+ofs_mp]];
-				old_page_attrs = collected[collected.defs[4+ofs_mp]];
-				if (old_version===92) {
-					// replace the pre tags with the new nowiki syntax
-					for (i=0;i<page_contents.length;i++) {
-						// page is encrypted, leave it as is
-						if (old_page_attrs[i] & 2)
-							continue;
-						// ignore special pages
-						if (page_names[i].indexOf("Special::")===0) {
-							++sys_pages;
-							continue;
-						}
-						page_contents[i] = page_contents[i].replace(/<pre(.*?)>((.|\n)*?)<\/pre>/g,
-										function (str, $1, $2) {
-											var s="{{{"+$2+"}}}";
-											if ($1.length)
-												s = "<span"+$1+">"+s+"</span>";
-											return s;
-										});
-					}
-					// done pre tags fixing
-				} // v0.9.2 only
-			} // do not import content pages
-
-			// since version v0.9.5B+ we have an object oriented WoaS
-			// load config from it
-			if (old_version >= 95) {
+	}
 				
-				var cfgStartMarker = 'woas["'+'config"] = {',
-				// grab the woas config definition
-					cfg_start = data.indexOf(cfgStartMarker),
-					cfg_found = false;
-				if (cfg_start !== -1) {
-					var cfg_end = data.indexOf('}', cfg_start+cfgStartMarker.length);
-					if (cfg_end !== -1) {
-						woas.config = woas.eval(data.substring(cfg_start+cfgStartMarker.length-1, cfg_end+1), true);
-						cfg_found = !woas.eval_failed;
-					}
-				}
-				
-				if (!cfg_found) {
-					woas.log("Failed to import old configuration object");
-				} else {
-					// add the new debug option
-					if (old_version<=107)
-						woas.config.debug_mode = old_cfg.debug_mode;
-					// add the new safe mode and WSIF DS options
-					if (old_version < 112) {
-						woas.config.safe_mode = old_cfg.safe_mode;
-						woas.config.wsif_author = old_cfg.wsif_author;
-						woas.config.wsif_ds = old_cfg.wsif_ds;
-						woas.config.wsif_ds_lock = old_cfg.wsif_ds_lock;
-						woas.config.wsif_ds_multi = old_cfg.wsif_ds_multi;
-					}
-					if (old_version < 120) {
-						woas.config.new_tables_syntax = old_cfg.new_tables_syntax;
-						woas.config.store_mts = old_cfg.store_mts;
-						woas.config.folding_style = old_cfg.folding_style;
-					}
-				} // !cfg_found
+	if (!cfg_found) {
+		woas.log("Failed to import old configuration object");
+	} else {
+		// add the new debug option
+		if (old_version<=107)
+			woas.config.debug_mode = old_cfg.debug_mode;
+		// add the new safe mode and WSIF DS options
+		if (old_version < 112) {
+			woas.config.safe_mode = old_cfg.safe_mode;
+			woas.config.wsif_author = old_cfg.wsif_author;
+			woas.config.wsif_ds = old_cfg.wsif_ds;
+			woas.config.wsif_ds_lock = old_cfg.wsif_ds_lock;
+			woas.config.wsif_ds_multi = old_cfg.wsif_ds_multi;
+		}
+		if (old_version < 120) {
+			woas.config.new_tables_syntax = old_cfg.new_tables_syntax;
+			woas.config.store_mts = old_cfg.store_mts;
+			woas.config.folding_style = old_cfg.folding_style;
+		}
+		// check for any undefined config property - for safety
+		for(p in woas.config) {
+			woas.log(p+": "+(typeof woas.config[p]));
+			if ((typeof woas.config[p] == "undefined") && (typeof old_cfg[p] != "undefined"))
+				woas.config[p] = old_cfg[p];
+		}
 
-			} // done importing config object
+	} // done importing config object
 		
-		// some GC help
-		data = null;
-	} // done importing from v0.9.2B+
+	// some GC help
+	data = null;
 
 	// modified timestamp for pages before 0.10.0
 	var current_mts = Math.round(new Date().getTime()/1000),
@@ -384,7 +303,7 @@ woas.import_wiki = function() {
 					update_hotkeys = true;
 				// allowed pages after above filtering are Plugins, Aliases, Hotkeys
 			} else if (page_names[i].indexOf("Special::")===0) {
-				if ((old_version>=94) && (old_version<=96)) {
+				if (old_version===96) {
 					if (page_names[i]=="Special::Bootscript") {
 						bootscript_code = page_contents[i];
 						pages_imported++;
@@ -440,32 +359,17 @@ woas.import_wiki = function() {
 					page_mts.push( this.config.store_mts ? old_page_mts[i] : 0);
 			} else { // page already existing
 //				log("replacing "+page_names[i]);	//log:0
-				if (old_version==94) {
-					// convert embedded files to base64 encoding
-					if (old_page_attrs[i] & 4) {
-						// plain copy images, which were already b64-encoded
-						if (old_page_attrs[i] & 8)
-							pages[pi] = page_contents[i];
-						else {
-							// page is encrypted, skip it
-							if (old_page_attrs[i] & 2)
-								continue;
-						}
-						pages[pi] = encode64(page_contents[i]);
-					}
-				} else {
-					page_titles[pi] = page_names[i];
-					// fix the trailing nul bytes bug in encrypted pages
-					if ((old_version>=102) && (old_version<=103)
-						&& (old_page_attrs[i] & 2)) {
-							var rest = page_contents[i].length % 16;
-							if (rest)
-								log("removing "+rest+" trailing bytes from page "+page_names[i]); //log:1
-							while (rest-- > 0) {page_contents[i].pop();}
-					}
-					// copy content
-					pages[pi] = page_contents[i];
+				page_titles[pi] = page_names[i];
+				// fix the trailing nul bytes bug in encrypted pages
+				if ((old_version>=102) && (old_version<=103)
+					&& (old_page_attrs[i] & 2)) {
+						var rest = page_contents[i].length % 16;
+						if (rest)
+						log("removing "+rest+" trailing bytes from page "+page_names[i]); //log:1
+						while (rest-- > 0) {page_contents[i].pop();}
 				}
+				// copy content
+				pages[pi] = page_contents[i];
 				page_attrs[pi] = old_page_attrs[i];
 			}
 			++pages_imported;
@@ -489,7 +393,6 @@ woas.import_wiki = function() {
 	if ((new_main_page !== old_cfg.main_page) && this.page_exists(new_main_page))
 		this.config.main_page = new_main_page;
 	
-	this.config.permit_edits = !old_block_edits;
 	} while (false); // fake do..while ends here
 	
 	// fix configuration for older versions
