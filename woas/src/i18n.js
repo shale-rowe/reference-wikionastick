93d2a3ec006bf77f11e1653ce3936f6f24b2852e
legolas558 <legolas558@dadf181f-332c-0410-97d6-92020e263e48>
2010-05-16 19:25:23 +0000

    * fixed import overwrite settings

--- woas/src/i18n.js
+++ woas/src/i18n.js
@@ -6,6 +6,7 @@ woas.i18n = {
 	ASK_MENU_LINK: "Do you want to add a link into the main menu?",
 	B64_REQ: " (requires %s due to base64 encoding)",
 	CANCEL_EDITING:"Changes to this page will not be saved.",
+	CONFIRM_OVERWRITE: "Page \"%s\" already exists. Overwrite it?",
 	CONFIRM_DELETE:"Are you sure you want to DELETE page '%s'?",
 	CONFIRM_DELETE_ALL1: "Are you going to ERASE all your pages?",
 	CONFIRM_DELETE_ALL2: "This is the last confirm needed in order to ERASE all your pages.\n\nALL YOUR PAGES WILL BE LOST\n\nAre you sure you want to continue?",
