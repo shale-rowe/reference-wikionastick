90645becfc2995afc705e609f3148184534d66b3
pvhl <pvhl@dadf181f-332c-0410-97d6-92020e263e48>
2011-01-08 22:24:05 +0000

    * slight grammatical change * macroTagThis and reformat

--- fix-wip/src/editor.js
+++ fix-wip/src/editor.js
@@ -44,34 +44,34 @@ TextAreaSelectionHelper.prototype.getSelectedText=function() {
 
 TextAreaSelectionHelper.prototype.setSelectedText=function(text, secondtag) {
 	if(this.iesel) {
-	if(typeof(secondtag)=="string") {
-	  var l=this.iesel.text.length;
-		 this.iesel.text=text+this.iesel.text+secondtag;
-	  this.iesel.moveEnd("character", -secondtag.length);
-	   this.iesel.moveStart("character", -l);   
+		if(typeof(secondtag)=="string") {
+			var l=this.iesel.text.length;
+			this.iesel.text=text+this.iesel.text+secondtag;
+			this.iesel.moveEnd("character", -secondtag.length);
+			this.iesel.moveStart("character", -l);   
+		} else {
+			this.iesel.text=text;
+		}
+		this.iesel.select();
+	} else if (this.start>=0 && this.end>=this.start) {
+		var left=this.target.value.substring(0,this.start);
+		var right=this.target.value.substr(this.end);
+		var scont=this.target.value.substring(this.start, this.end);
+		if(typeof(secondtag)=="string") {
+			this.target.value=left+text+scont+secondtag+right;
+			this.end=this.target.selectionEnd=this.start+text.length+scont.length;
+			this.start=this.target.selectionStart=this.start+text.length;    
+		} else {
+			this.target.value=left+text+right;
+			this.end=this.target.selectionEnd=this.start+text.length;
+			this.start=this.target.selectionStart=this.start+text.length;
+		}
+		this.target.scrollTop=this.scroll;
+		this.target.focus();
 	} else {
-	  this.iesel.text=text;
+		this.target.value += text + ((typeof(secondtag)=="string")? secondtag: "");
+		if(this.scroll>=0) this.target.scrollTop=this.scroll;
 	}
-	this.iesel.select();
- } else if(this.start>=0&&this.end>=this.start) {
-    var left=this.target.value.substring(0,this.start);
-    var right=this.target.value.substr(this.end);
- var scont=this.target.value.substring(this.start, this.end);
- if(typeof(secondtag)=="string") {
-   this.target.value=left+text+scont+secondtag+right;
-   this.end=this.target.selectionEnd=this.start+text.length+scont.length;
-   this.start=this.target.selectionStart=this.start+text.length;    
- } else {
-      this.target.value=left+text+right;
-   this.end=this.target.selectionEnd=this.start+text.length;
-   this.start=this.target.selectionStart=this.start+text.length;
- }
- this.target.scrollTop=this.scroll;
- this.target.focus();
- } else {
-   this.target.value+=text + ((typeof(secondtag)=="string")? secondtag: "");
-if(this.scroll>=0) this.target.scrollTop=this.scroll;
- }
 };
 
 TextAreaSelectionHelper.prototype.getText=function() {
@@ -103,6 +103,11 @@ function TagThis(starttag, endtag){
 
 function FullTagThis(tag){
 	woas._editor.setSelectedText('<'+tag+'>','<'+'/'+tag+'>');
+}
+
+function macroTagThis(tagone, tagtwo) {
+	tagtwo = typeof tagtwo === 'undefined' ? '' : '("' + tagtwo + '")'
+	TagThis('<<<' + tagone + tagtwo + ':\n', '\n>>>');
 }
 
 function setUrl(starttag,centertag,endtag) {
